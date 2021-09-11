9e2b9fa7d63a86c75bc1357a665c648739e2c0f2
abdul riyaz <45689131+abdulriiyaz@users.noreply.github.com>
2021-09-11 19:32:55 +0530

    Working chat, needs polishing and details

--- server.js
+++ server.js
@@ -1,17 +1,45 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
+const socketio = require('socket.io');
 
 const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+
+
+
+//run when client connects
 
 const PORT = 3000 || process.env.PORT;
 
 //Setting static folder
 app.use(express.static(path.join(__dirname,'public')))
 
+io.on('connection',socket => {
+    // console.log("New Connection...")
+
+    //when client connects
+    socket.emit('message', "Welcome to the App.");
+
+    //broadcast when client joins
+    socket.broadcast.emit('message', "User has Joined!");
+
+    //when client disonnects.
+    socket.on('disconnect', () => {
+        io.emit('message', "User has left the chat.");
+    });
+
+    //listen for chat messages
+    socket.on('chatMessage', msg => {
+        console.log(msg);
+        io.emit('message', msg);
+    })
+});
 
 
 
 //Server Running
-app.listen(PORT, () => {
+server.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
